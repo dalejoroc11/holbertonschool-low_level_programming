dbb8c5d1369b2a0be005f72b4b438e9447d15143
dalejoroc11 <diegorojas279@hotmail.com.com>
2020-02-06 20:12:05 +0000

    6

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,15 @@
+# include <stdio.h>
+/**
+ *main- print numbers
+ *Return: 0
+ */
+int main(void)
+{
+char numbers;
+for (numbers = 48 ; numbers <= 57; numbers++)
+{
+putchar(numbers);
+}
+putchar('\n');
+return (0);
+}
