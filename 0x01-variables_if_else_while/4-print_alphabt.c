6e9d208b94af14614d01c82b3a96d69a107bb114
dalejoroc11 <diegorojas279@hotmail.com.com>
2020-02-06 20:04:57 +0000

    low level

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -6,19 +6,10 @@
 int main(void)
 {
 char al1;
-char al2;
-char al3;
-for (al1 = 'a'; al1 < 'e'; al1++)
+for (al1 = 'a'; al1 <= 'z'; al1++)
 {
+if (al1 != 'e' && al1 != 'q')
 putchar(al1);
-}
-for (al2 = 'f'; al2 < 'q'; al2++)
-{
-putchar(al2);
-}
-for (al3 = 'r'; al3 <= 'z'; al3++)
-{
-putchar(al3);
 }
 putchar('\n');
 return (0);
