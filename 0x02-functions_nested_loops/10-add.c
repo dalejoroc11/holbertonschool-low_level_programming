123788be66b7bd2ba0c9513d598053e43db6f3ae
dalejoroc11 <diegorojas279@hotmail.com.com>
2020-02-11 16:31:04 +0000

    add two numbers

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,13 @@
+#include "holberton.h"
+/**
+ * add - add two numbers
+ * Return: result add
+ * @n1: number one
+ * @n2: number two
+ */
+int add(int n1, int n2)
+{
+int r;
+r = n1 + n2;
+return (r);
+}
