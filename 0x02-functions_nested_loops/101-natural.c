77a97279ee2df3fa509c5c77ca107023fb39cc05
dalejoroc11 <diegorojas279@hotmail.com.com>
2020-02-14 02:50:59 +0000

    more functions

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -1,5 +1,5 @@
-#include <stdio.h>
 #include "holberton.h"
+#include <stdio.h>
 /**
  *main - value of an integer
  *Return: 0
@@ -8,11 +8,11 @@ int main(void)
 {
 int sum = 0;
 int ite;
-for (ite = 1; a < 1024; a++)
+for (ite = 1; ite < 1024; ite++)
 {
 if ((ite % 3 == 0 || (ite % 5 == 0))
 {
-num = num + ite;
+sum = sum + ite;
 }
 }
 printf("%d\n", sum);
