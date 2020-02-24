e03406e0a9e87003b8c119e32782f575dcabdc20
dalejoroc11 <dalejoro279@hotmail.com>
2020-02-24 15:12:51 +0000

    more pointers

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,12 @@
+#include "holberton.h"
+#include <stdio.h>
+/**
+ * *_memset - fill memory
+ * @b: byte const
+ * @n: bytes
+ * @s: memory señalada
+ * Return: s
+ */
+char *_memset(char *s, char b, unsigned int n)
+{
+}
