01f8dbbd629909fa77832d7f3fb9de74b03f1b78
dalejoroc11 <dalejoro279@hotmail.com>
2020-02-24 21:10:27 +0000

    pointers

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,21 @@
+#include "holberton.h"
+#include <string.h>
+/**
+ * *_strchr - localice a char
+ * @s: string
+ * @c: char
+ * Return: s, null
+ */
+char *_strchr(char *s, char c)
+{
+int i;
+for (i = 0; s[i] != '\0'; i++)
+{
+if (s[i] == c)
+{
+s = &s[i];
+return (s);
+}
+}
+return ('\0');
+}
