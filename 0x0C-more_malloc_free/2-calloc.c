224d4b9720eda13867b168b77bc4ba924069f627
dalejoroc11 <dalejoro279@hotmail.com>
2020-03-05 19:36:11 +0000

    calloc

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -2,12 +2,23 @@
 #include <stdio.h>
 #include <stdlib.h>
 /**
- * -
- * @
+ * _calloc - mem asign
+ * @nmemb : men
+ * @size: size l
  * Return: sucess
  */
-
+void *_calloc(unsigned int nmemb, unsigned int size)
 {
-
-return (0);
+char *p;
+unsigned int i = 0;
+if (nmemb == 0 || size == 0)
+return ('\0');
+p = malloc(nmemb * size);
+if (p == '\0')
+return ('\0');
+for (i = 0; i < (nmemb * size); i++)
+{
+p[i] = 0;
+}
+return (p);
 }
