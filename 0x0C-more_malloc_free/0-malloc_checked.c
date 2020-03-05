862d2431f353a1e5d2977ff0cffda1f6fcdc81f8
dalejoroc11 <dalejoro279@hotmail.com>
2020-03-05 17:57:00 +0000

    malloc

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -2,12 +2,18 @@
 #include <stdio.h>
 #include <stdlib.h>
 /**
- * -
- * @
+ * malloc_checked - alloc mem malloc
+ * @b
  * Return: sucess
  */
-
+void *malloc_checked(unsigned int b)
 {
-
-return (0);
+int *p;
+p = NULL;
+p = malloc(b);
+if (p == NULL)
+{
+exit (98);
+}
+return (p);
 }
