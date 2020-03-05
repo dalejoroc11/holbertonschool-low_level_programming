4a316def47fc290dc20a4a50f935ee7cd73aaa60
dalejoroc11 <dalejoro279@hotmail.com>
2020-03-05 16:18:45 +0000

    commit

---
+++ 0x0C-more_malloc_free/101-mul.c
@@ -0,0 +1,25 @@
+#include "holberton.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * -
+ * @
+ * Return: sucess
+ */
+
+{
+
+return (0);
+}#include "holberton.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * -
+ * @
+ * Return: sucess
+ */
+
+{
+
+return (0);
+}
