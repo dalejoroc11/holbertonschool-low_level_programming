fcd6941ccc1b27104a91414dfb02bb2b312428d8
dalejoroc11 <diegorojas279@hotmail.com.com>
2020-03-06 22:59:38 +0000

    functions

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -1,20 +1,25 @@
-#include "holberton.h"
 #include <stdio.h>
+
 /**
- * main - value of an integer
- * Return: 0
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
  */
+
 int main(void)
 {
-int sum = 0;
-int ite;
-for (ite = 1; ite < 1024; ite++)
-{
-if ((ite % 3 == 0 || (ite % 5 == 0))
-{
-sum = sum + ite;
-}
-}
-printf("%d\n", sum);
-return (0);
+	int i, ac = 0;
+
+	for (i = 0; i < 1024; i++)
+	{
+		if ((i % 3 == 0) || (i % 5 == 0))
+		{
+			ac = ac + i;
+		}
+
+	}
+
+	printf("%d\n", ac);
+
+	return (0);
 }
