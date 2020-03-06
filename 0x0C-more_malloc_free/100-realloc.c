4b1a08b177362bd012551d4e4089916883f357f0
dalejoroc11 <diegorojas279@hotmail.com.com>
2020-03-06 02:39:49 +0000

    realloc

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -2,24 +2,38 @@
 #include <stdio.h>
 #include <stdlib.h>
 /**
- * -
- * @
+ * _realloc - fun
+ * @new_size: var
+ * @old_size: var
+ * @ptr: pointer
  * Return: sucess
  */
-
+void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 {
-
-return (0);
-}#include "holberton.h"
-#include <stdio.h>
-#include <stdlib.h>
-/**
- * -
- * @
- * Return: sucess
- */
-
+char *p;
+unsigned int i = 0;
+if (new_size == old_size)
+return (ptr);
+if (new_size == 0 && ptr)
 {
-
-return (0);
+free(ptr);
+return ('\0');
+}
+if (!ptr)
+{
+p = malloc(new_size);
+if (!p)
+return ('\0');
+return (p);
+}
+p = malloc(new_size);
+if (p == '\0')
+return ('\0');
+while (i < old_size && i < new_size)
+{
+p[i] = ((char *)ptr)[i];
+i++;
+}
+free(ptr);
+return (p);
 }
--- 0x0C-more_malloc_free/3-array_range.c
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -2,12 +2,27 @@
 #include <stdio.h>
 #include <stdlib.h>
 /**
- * -
- * @
+ * array_range - create array
+ * @min: var min
+ * @max: var max
  * Return: sucess
  */
-
+int *array_range(int min, int max)
 {
-
-return (0);
+int *p;
+int i = 0, j = 0;
+if (min > max)
+return ('\0');
+i = max - min + 1;
+p = malloc(sizeof(int) * i);
+if (!p)
+return ('\0');
+while (min != max)
+{
+p[j] = min;
+j++;
+min++;
+}
+p[j] = max;
+return (p);
 }
