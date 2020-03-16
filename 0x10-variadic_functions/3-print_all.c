221fcc8d24109a9a386b001170fa8be5ac4f71ff
dalejoroc11 <diegorojas279@hotmail.com.com>
2020-03-16 00:58:08 +0000

    variadic functions

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -1,49 +1,84 @@
 #include "variadic_functions.h"
 /**
- *print_all - fun print everything
- *@: pointer const
- *@format: const
- *Return: 0
- */
+ * p_char - Prints a character
+ * @valist: va_list
+ * Return: none
+ **/
+void p_char(va_list valist)
+{
+	printf("%c", va_arg(valist, int));
+}
+/**
+ * p_float - Prints a float
+ * @valist: va_list
+ * Return: none
+ **/
+void p_float(va_list valist)
+{
+	printf("%f", va_arg(valist, double));
+}
+/**
+ * p_int - Prints a int
+ * @valist: va_list
+ * Return: none
+ **/
+void p_int(va_list valist)
+{
+	printf("%d", va_arg(valist, int));
+}
+/**
+ * p_str - Prints a str
+ * @valist: va_list
+ * Return: none
+ **/
+void p_str(va_list valist)
+{
+	char *str;
+
+	str = va_arg(valist, char *);
+
+	if (str == 0)
+		str = "(nil)";
+	printf("%s", str);
+}
+/**
+ * print_all - prints "anything"
+ * @format:  list of types of arguments
+ *
+ * Return: none
+ **/
 void print_all(const char * const format, ...)
 {
-va_list valist;/*define valist*/
-int b = 0, c = 0, a = 0;/*define parameters*/
-char *aa;/*define`pouinter*/
-while (format[b])
-b++;/*cointer*/
-va_start(valist, format);
-while (c < b)
-{
-switch (format[c])/*switch*/
-{
-case 'c':
-printf("%c", va_arg(valist, int));
-a = 1;
-break;
-case 'i':
-printf("%d", va_arg(valist, int));
-a = 1;
-break;
-case 'f':
-printf("%f", va_arg(valist, double));
-a = 1;
-break;
-case 's':
-aa = va_arg(valist, char *);
-if (aa == NULL)
-aa = "(nil)";
-printf("%s", aa);
-a = 1;
-break;
-}
-if (c < b - 1 && a == 1)
-{
-a = 0;
-printf(", ");
-}
-c++;
-}
-printf("\n");
-va_end(valist);
+	int i, j;
+	va_list valist;
+	char *separator;
+	p_func p_types[] = {
+		{"c", p_char},
+		{"f", p_float},
+		{"i", p_int},
+		{"s", p_str},
+		{NULL, NULL}
+	};
+
+	va_start(valist, format);
+	i = 0;
+	separator = "";
+
+	while (format != NULL && format[i] != 0)
+	{
+		j = 0;
+		while (p_types[j].f != NULL)
+		{
+			if (p_types[j].tp[0] == format[i])
+			{
+				printf("%s", separator);
+				separator = ", ";
+				p_types[j].f(valist);
+			}
+			j++;
+		}
+		i++;
+	}
+	va_end(valist);
+	printf("\n");
 }
