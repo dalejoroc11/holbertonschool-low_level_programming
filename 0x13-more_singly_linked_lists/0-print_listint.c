25d9d2e8165697687d0a8ad35b223cd05987f0f6
dalejoroc11 <diegorojas279@hotmail.com.com>
2020-03-24 16:53:10 +0000

    lists

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -1,13 +1,12 @@
 #include "lists.h"
 /**
- * print_listint - print list 
- * @h: pointer head 
- * Return: Always 0.
+ * print_listint - print list
+ * @h: pointer head
+ * Return: Always 0
  */
 size_t print_listint(const listint_t *h)
 {
-int len;
-lent = 0;
+int len = 0;
 while (h != NULL)
 {
 printf("%d\n", h[0].n);/*print format call structure*/
