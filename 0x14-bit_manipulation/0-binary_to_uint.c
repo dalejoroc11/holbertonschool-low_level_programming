79d58d651b4dc41ae7cc9a31332a3ed1c2242866
dalejoroc11 <diegorojas279@hotmail.com.com>
2020-03-26 19:34:50 +0000

    bit

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -6,6 +6,34 @@
  */
 unsigned int binary_to_uint(const char *b)
 {
-
+unsigned int num = 0;
+int i = 0;
+int p = 2;
+if (b == NULL)
+{
 return (0);
+}
+while (b[i] != '\0')
+{
+if (b[i] != 48 && b[i] != 49)
+{
+return (0);
+}
+i++;
+}
+
+i--;
+
+if (b[i] == 49)
+{
+num = 1;
+i--;
+}
+while (i >= 0)
+{
+num = num + (p * (b[i] - 48));
+i--;
+p += p;
+}
+return (num);
 }
