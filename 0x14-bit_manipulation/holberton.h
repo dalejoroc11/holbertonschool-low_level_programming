68f5059412afc54de300aea3520e431b047cef83
dalejoroc11 <diegorojas279@hotmail.com.com>
2020-03-26 20:51:56 +0000

    bit

--- 0x14-bit_manipulation/holberton.h
+++ 0x14-bit_manipulation/holberton.h
@@ -1,6 +1,7 @@
 #ifndef HOLBERTON_H
 #define HOLBERTON_H
 #include <stdio.h>
+#include <stdint.h>
 unsigned int binary_to_uint(const char *b);
 int _putchar(char c);
 void print_binary(unsigned long int n);
