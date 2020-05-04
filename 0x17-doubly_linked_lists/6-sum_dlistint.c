29b05c8cd204bae9ada8fbb6ae6da8f7861db1fc
dalejoroc11 <diegorojas11@hotmail.com>
2020-05-04 03:06:03 +0000

    finish

--- 0x17-doubly_linked_lists/6-sum_dlistint.c
+++ 0x17-doubly_linked_lists/6-sum_dlistint.c
@@ -6,11 +6,11 @@
  */
 int sum_dlistint(dlistint_t *head)
 {
-  int res = 0;
-  while (head)
-    {
-      res += head->n;
-      head = head->next;
-    }
-  return (res);
+int res = 0;
+while (head)
+{
+res += head->n;
+head = head->next;
+}
+return (res);
 }
