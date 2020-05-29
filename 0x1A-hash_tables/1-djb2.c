5cb70ec1294bd092f57cd92c187f837aae8ccc39
dalejoroc11 <diegorojas279@hotmail.com>
2020-05-29 18:03:52 +0000

    fix

--- 0x1A-hash_tables/1-djb2.c
+++ 0x1A-hash_tables/1-djb2.c
@@ -1,12 +1,19 @@
+#include "hash_tables.h"
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+/**
+ * hash_djb2 - distribution of the keys and fewer splits
+ * @str: string calculate hash
+ * Return: Calculate ht
+ */
 unsigned long int hash_djb2(const unsigned char *str)
 {
-    unsigned long int hash;
-    int c;
-
-    hash = 5381;
-    while ((c = *str++))
-    {
-        hash = ((hash << 5) + hash) + c; /* hash * 33 + c */
-    }
-    return (hash);
+unsigned long int hash;
+int c;
+hash = 5381;
+while ((c = *str++))
+{
+hash = ((hash << 5) + hash) + c; /* hash * 33 + c */
 }
+return
--- 0x1A-hash_tables/2-key_index.c
+++ 0x1A-hash_tables/2-key_index.c
@@ -1,11 +1,14 @@
 #include "hash_tables.h"
-
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
 /**
- * key_index -
- *
- * Return: 
+ * key_index - get index to djb2
+ * @size: size arrays
+ * @key: key get ind
+ * Return: ind
  */
 unsigned long int key_index(const unsigned char *key, unsigned long int size)
 {
-
+return (hash_djb2(key) % size);
 }
